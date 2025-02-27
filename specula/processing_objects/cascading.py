7f3637784686cd81a54c27425175b0003acc583b
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-02-27 11:02:52 +0100

    Added ALPAO command projection to cascading object

--- specula/processing_objects/cascading.py
+++ specula/processing_objects/cascading.py
@@ -7,12 +7,14 @@ from specula.data_objects.electric_field import ElectricField
 class Cascading(BaseProcessingObj):
 
     def __init__(self,
+                 nmodes,
                  size,       # output image size
                  target_device_idx=None,
                  precision=None
                 ):
         super().__init__(target_device_idx=target_device_idx, precision=precision)
 
+        self.nmodes = nmodes
         self.pixels = Pixels(size[0], size[1], target_device_idx=target_device_idx)
         self.inputs['in_ef'] = InputValue(type=ElectricField)
         self.outputs['out_pixels'] = self.pixels
@@ -21,6 +23,9 @@ class Cascading(BaseProcessingObj):
         # self.local_inputs dictionary has a one-to-on correspondence with self.inputs
         # and its content is automatically set up before trigger code is invoked
         # do the computation here
+        phase = self.local_inputs['in_ef'].phaseInNm
+      #  print(f'{phase.shape} @ {self.recmat.shape}')
+        alpao_cmd = phase.reshape((phase.shape[0]*phase.shape[1],)) @ self.recmat
         self.pixels.pixels *= 0
 
     def post_trigger(self):
@@ -28,4 +33,10 @@ class Cascading(BaseProcessingObj):
         self.pixels.generation_time = self.current_time
 
 
+    def setup(self, dt, niters):
+        super().setup(dt, niters)
+
+        in_ef = self.inputs['in_ef'].get(target_device_idx=self.target_device_idx)
+        nelements = in_ef.size[0] * in_ef.size[1]
+        self.recmat = self.xp.zeros((nelements, self.nmodes))
 
