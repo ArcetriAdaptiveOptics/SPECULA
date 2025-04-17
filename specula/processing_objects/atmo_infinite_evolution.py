1b81af6bc0742679ea5e05d02628bc3460fc1686
Guido Agapito <guido.agapito@inaf.it>
2025-04-17 12:25:21 +0200

    (GA) scale bugfix and L0 as list

--- specula/processing_objects/atmo_infinite_evolution.py
+++ specula/processing_objects/atmo_infinite_evolution.py
@@ -61,7 +61,7 @@ class InfinitePhaseScreen(BaseDataObj):
         self.setup()
 
     def phase_covariance(self, r, r0, L0):
-        r = self.xp.asnumpy(r)
+        r = cpuArray(r)
         r0 = float(r0)
         L0 = float(L0)
         # Get rid of any zeros
@@ -75,7 +75,7 @@ class InfinitePhaseScreen(BaseDataObj):
 #        C = (((2 * self.xp.pi * r) / L0) ** (5. / 6)) * kv(5. / 6, (2 * self.xp.pi * r) / L0)
 #        cov = A * B1 * B2 * C / 2
         
-        cov = self.xp.asarray(cov) / 2
+        cov = self.xp.asarray(cov)
 
         return cov
 
@@ -285,6 +285,12 @@ class AtmoInfiniteEvolution(BaseProcessingObj):
             self.pixel_layer_size = np.full_like(self.heights, int(fov_in_m / self.pixel_pitch / 2.0) * 2)
         
         self.L0 = L0
+        
+        if np.isscalar(self.L0):
+            self.L0 = [self.L0] * len(self.heights)
+        elif len(self.L0) != len(self.heights):
+            raise ValueError(f"L0 must have the same length as heights ({len(self.heights)}), got {len(self.L0)}")
+        
         self.Cn2 = np.array(Cn2, dtype=self.dtype)
         self.pixel_pupil = pixel_pupil
         self.data_dir = data_dir
