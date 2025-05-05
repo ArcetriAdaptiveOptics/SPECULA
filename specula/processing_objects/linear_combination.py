0ab3188449eeaa843039fcbad9e6fba2d2a8a1fa
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-05-05 16:57:12 +0200

    LGS-only linear combination

--- specula/processing_objects/linear_combination.py
+++ specula/processing_objects/linear_combination.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 from specula.base_processing_obj import BaseProcessingObj
 from specula.base_value import BaseValue
 from specula.connections import InputList
@@ -12,6 +12,19 @@ class LinearCombination(BaseProcessingObj):
         super().__init__(target_device_idx=target_device_idx, precision=precision)       
 
         self.inputs['in_vectors_list'] = InputList(type=BaseValue)
-        
-        self._out_vector = BaseValue()
-        self.outputs['out_vector'] = self._out_vector
+        self.out_vector = BaseValue()
+        self.outputs['out_vector'] = self.out_vector
+
+    def trigger_code(self):
+        in_vectors = self.local_inputs['in_vectors_list']
+        lgs = in_vectors[0].value
+        focus = in_vectors[1].value
+        lift = in_vectors[2].value
+        ngs = in_vectors[3].value
+
+        focus *= 0
+        lift *= 0
+        ngs *= 0
+
+        self.out_vector.value = np.concatenate([lgs, focus, lift, ngs])
+        self.out_vector.generation_time = self.current_time
