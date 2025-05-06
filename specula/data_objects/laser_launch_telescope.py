a83f1cbb94abbb5cbfb70ce0a8ea4e4df3ea867e
Guido Agapito <guido.agapito@inaf.it>
2025-05-06 14:10:08 +0200

    (GA) tel_position

--- specula/data_objects/laser_launch_telescope.py
+++ specula/data_objects/laser_launch_telescope.py
@@ -1,21 +1,33 @@
-
 from specula.base_processing_obj import BaseProcessingObj
 
 class LaserLaunchTelescope(BaseProcessingObj):
-    '''Laser Launch Telescope'''
+    '''
+    Laser Launch Telescope
+    
+    args:
+    ----------
+    spot_size : float
+        The size of the laser spot in arcsec.
+    tel_position : list
+        The x, y and z position of the launch telescope w.r.t. the telescope in m.
+    beacon_focus : float
+        The distance from the telescope pupil to beacon focus in m.
+    beacon_tt : list
+        The tilt and tip of the beacon in arcsec.
+    '''
 
     def __init__(self,
                  spot_size: float = 0.0,
-                 position: list = [],
+                 tel_position: list = [],
                  beacon_focus: float = 90e3,
                  beacon_tt: list = [],
                  target_device_idx: int = None, 
                  precision: int = None
         ):
 
-        super().__init__(target_device_idx=target_device_idx, precision=precision) 
+        super().__init__(target_device_idx=target_device_idx, precision=precision)
 
         self.spot_size = spot_size
-        self.tel_pos = position
+        self.tel_pos = tel_position
         self.beacon_focus = beacon_focus
         self.beacon_tt = beacon_tt
