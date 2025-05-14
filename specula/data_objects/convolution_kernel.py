a0806ceee6c6fd0a3dc669d94a37448ce5ff54b2
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-05-14 12:27:05 +0200

    ConvolutionKernel is now single precision

--- specula/data_objects/convolution_kernel.py
+++ specula/data_objects/convolution_kernel.py
@@ -13,7 +13,7 @@ import hashlib, json
 def lgs_map_sh(nsh, diam, rl, zb, dz, profz, fwhmb, ps, ssp,
                overs=2, theta=[0.0, 0.0], rprof_type=0,
                mask_pupil=False, pupil_weight=None, doCube=True,
-               xp=np):
+               dtype=np.float32, xp=np):
     """
     It returns the pattern of Sodium Laser Guide Star images relayed by a Shack-Hartman lenlet array.
     Only geometrical propagation is taken in account (no diffraction effects).
@@ -45,39 +45,32 @@ def lgs_map_sh(nsh, diam, rl, zb, dz, profz, fwhmb, ps, ssp,
     theta = cpuArray(theta)
     # Oversampling and lenslet grid setup
     ossp = ssp * overs
-    xsh, ysh = xp.meshgrid(xp.linspace(-diam / 2, diam / 2, nsh), xp.linspace(-diam / 2, diam / 2, nsh))
-    xfov, yfov = xp.meshgrid(xp.linspace(-ssp * ps / 2, ssp * ps / 2, ossp), xp.linspace(-ssp * ps / 2, ssp * ps / 2, ossp))   
+
+    xsh, ysh = xp.meshgrid(xp.linspace(-diam / 2, diam / 2, nsh, dtype=dtype),
+                           xp.linspace(-diam / 2, diam / 2, nsh, dtype=dtype))
+    xfov, yfov = xp.meshgrid(xp.linspace(-ssp * ps / 2, ssp * ps / 2, ossp, dtype=dtype),
+                             xp.linspace(-ssp * ps / 2, ssp * ps / 2, ossp, dtype=dtype))
     # Gaussian parameters for the sodium layer
     sigma = (fwhmb * ASEC2RAD * zb) / (2 * xp.sqrt(2 * xp.log(2)))
     one_over_sigma2 = 1.0 / sigma**2
     exp_sigma = -0.5 * one_over_sigma2   
-    rb = xp.array([theta[0] * ASEC2RAD * zb, theta[1] * ASEC2RAD * zb, 0])
-    kv = xp.array([0, 0, 1])
-    BL = zb * kv + rb - xp.array(rl)
+    rb = xp.array([theta[0] * ASEC2RAD * zb, theta[1] * ASEC2RAD * zb, 0], dtype=dtype)
+    kv = xp.array([0, 0, 1], dtype=dtype)
+    BL = zb * kv + rb - xp.array(rl, dtype=dtype)
     el = BL / BL[2]
     # Create the focal plane field positions (rf) and the sub-aperture positions (rs)
-    rs_x, rs_y, rs_z = xsh, ysh, xp.zeros((nsh, nsh))
+    rs_x, rs_y = xsh, ysh
 
     rf_x = xp.tile(xfov * ASEC2RAD * zb, (nsh, nsh)).reshape(ossp * nsh, ossp * nsh)
     rf_y = xp.tile(yfov * ASEC2RAD * zb, (nsh, nsh)).reshape(ossp * nsh, ossp * nsh)
-    rf_z = xp.zeros((ossp * nsh, ossp * nsh))
 
     # Distance and direction vectors for calculating intensity maps
-    fs_x = rf_x - xp.repeat(xp.repeat(rs_x, ossp, axis=0), ossp, axis=1)
-    fs_y = rf_y - xp.repeat(xp.repeat(rs_y, ossp, axis=0), ossp, axis=1)
-    fs_z = zb + rf_z - xp.repeat(xp.repeat(rs_z, ossp, axis=0), ossp, axis=1)
-
-    es_x = fs_x / fs_z
-    es_y = fs_y / fs_z
-    es_z = fs_z / fs_z
-
-    # These arrays are very big, try to save some memory
-    del fs_x
-    del fs_y
-    del fs_z
+    es_x = (rf_x - xp.repeat(xp.repeat(rs_x, ossp, axis=0), ossp, axis=1)) / zb
+    es_y = (rf_y - xp.repeat(xp.repeat(rs_y, ossp, axis=0), ossp, axis=1)) / zb
+    es_z = 1.0
 
     # Initialize the field map (fmap) for LGS patterns
-    fmap = xp.zeros((nsh * ossp, nsh * ossp))
+    fmap = xp.zeros((nsh * ossp, nsh * ossp), dtype=dtype)
     nz = len(dz)
     # Gaussian or top-hat profile choice for LGS beam
     if rprof_type == 0:
@@ -92,10 +85,10 @@ def lgs_map_sh(nsh, diam, rl, zb, dz, profz, fwhmb, ps, ssp,
         if profz[iz] > 0:
             d2 = ((rf_x + dz[iz] * es_x - (rb[0] + dz[iz] * el[0]))**2 +
                   (rf_y + dz[iz] * es_y - (rb[1] + dz[iz] * el[1]))**2 +
-                  (rf_z + dz[iz] * es_z - (rb[2] + dz[iz] * el[2]))**2)
+                  ( 0.0 + dz[iz] * es_z - (rb[2] + dz[iz] * el[2]))**2)
 
             if rprof_type == 0:
-                fmap += (gnorm * profz[iz]) * xp.exp(d2 * exp_sigma)
+                fmap += (gnorm * profz[iz]) * xp.exp(d2 * exp_sigma, dtype=dtype)
             elif rprof_type == 1:
                 fmap += (gnorm * profz[iz]) * ((d2 * one_over_sigma2) <= 1.0)
 
