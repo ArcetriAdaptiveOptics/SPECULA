e1b402d8b7a26f19e7963db71370e7010958fbd9
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-06-16 14:51:26 +0200

    Updated test docstrings

--- test/test_base_data_obj.py
+++ test/test_base_data_obj.py
@@ -13,7 +13,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None, 'GPU not available')
     def test_copy_from_cpu_to_gpu(self):
-        
+        '''
+        Test that copyTo() with target_device_idx >= 0
+        allocates a new object on the GPU with the correct contents
+        '''
         a = BaseValue(value=np.arange(2), target_device_idx=-1)
         b = a.copyTo(target_device_idx=0)
 
@@ -23,7 +26,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None, 'GPU not available')
     def test_update_from_cpu_to_gpu(self):
-        
+        '''
+        Test that transferDataTo() with a GPU object
+        correctly updates the contents
+        '''
         a1 = BaseValue(value=np.arange(2), target_device_idx=-1)
         a2 = BaseValue(value=np.arange(2)+2, target_device_idx=-1)
         b = a1.copyTo(target_device_idx=0)
@@ -35,7 +41,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None, 'GPU not available')
     def test_copy_from_gpu_to_cpu(self):
-        
+        '''
+        Test that copyTo() with target_device_idx == -1
+        allocates a new object on the CPU with the correct contents
+        '''
         a = BaseValue(value=cp.arange(2), target_device_idx=0)
         b = a.copyTo(target_device_idx=-1)
 
@@ -45,7 +54,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None, 'GPU not available')
     def test_update_from_gpu_to_cpu(self):
-        
+        '''
+        Test that transferDataTo() with a CPU object
+        correctly updates the contents
+        '''
         a1 = BaseValue(value=cp.arange(2), target_device_idx=0)
         a2 = BaseValue(value=cp.arange(2)+2, target_device_idx=0)
         b = a1.copyTo(target_device_idx=-1)
@@ -57,7 +69,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None or cp.cuda.runtime.getDeviceCount() < 2, 'at least 2 GPUs are needed')
     def test_copy_from_gpu_to_gpu(self):
-        
+        '''
+        Test that copyTo() from a GPU device to another
+        allocates a new object on the target GPU with the correct contents
+        '''        
         a = BaseValue(value=cp.arange(2), target_device_idx=0)
         b = a.copyTo(target_device_idx=1)
 
@@ -67,7 +82,10 @@ class TestBaseDataObj(unittest.TestCase):
 
     @unittest.skipIf(cp is None or cp.cuda.runtime.getDeviceCount() < 2, 'at least 2 GPUs are needed')
     def test_update_from_gpu_to_gpu(self):
-        
+        '''
+        Test that transferDataTo() from a GPU device to another
+        correctly updates the contents
+        '''
         a1 = BaseValue(value=cp.arange(2), target_device_idx=0)
         a2 = BaseValue(value=cp.arange(2)+2, target_device_idx=0)
         b = a1.copyTo(target_device_idx=1)
