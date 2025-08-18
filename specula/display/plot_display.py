abd71e10aa40142e323b145d500dfa4fd4426794
Guido Agapito <guido.agapito@inaf.it>
2025-08-18 11:14:54 +0200

    (GA) update

--- specula/display/plot_display.py
+++ specula/display/plot_display.py
@@ -57,15 +57,16 @@ class PlotDisplay(BaseDisplay):
                 self._history[:-1] = self._history[1:]
             else:
                 self._history[:-1, :] = self._history[1:, :]
+            #self._count = n - 1
             if self._x_axis == 'time':
                 self._time_history = self._time_history[1:]
 
         # X axis for current data
         if self._x_axis == 'time':
             self._time_history.append(self.current_time_seconds)
-            x = np.array(self._time_history)
         else:
-            x = np.arange(self._count + 1)
+            self._time_history.append(self._count+1)
+        x = np.array(self._time_history)
 
         # Update data and plots
         xmin, xmax, ymin, ymax = [], [], [], []
