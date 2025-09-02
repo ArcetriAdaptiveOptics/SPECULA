f6321a588d0500bac5abf884b88ddef9a52abcae
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-09-02 15:38:44 +0200

    Suppressed expected warnings during tests

--- test/test_display.py
+++ test/test_display.py
@@ -3,6 +3,7 @@ import os
 import specula
 specula.init(0)  # Default target device
 
+import pytest
 import unittest
 
 import numpy as np
@@ -35,6 +36,7 @@ class TestDisplays(unittest.TestCase):
         self.pixel_pitch = 0.1
         self.S0 = 1.0
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_phase_display_init_and_trigger(self, target_device_idx, xp):
         """Test PhaseDisplay initialization and trigger"""
@@ -58,6 +60,7 @@ class TestDisplays(unittest.TestCase):
 
         display.close()
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_pixels_display_init_and_trigger(self, target_device_idx, xp):
         """Test PixelsDisplay initialization and trigger"""
@@ -81,6 +84,7 @@ class TestDisplays(unittest.TestCase):
 
         display.close()
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_slopec_display_init_and_trigger(self, target_device_idx, xp):
         """Test SlopecDisplay initialization and trigger"""
@@ -104,6 +108,7 @@ class TestDisplays(unittest.TestCase):
 
         display.close()
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_psf_display_init_and_trigger(self, target_device_idx, xp):
         """Test PsfDisplay initialization and trigger"""
@@ -135,6 +140,7 @@ class TestDisplays(unittest.TestCase):
 
         display.close()
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_modes_display_trigger(self, target_device_idx, xp):
         """Test ModesDisplay trigger functionality"""
@@ -160,6 +166,7 @@ class TestDisplays(unittest.TestCase):
 
         display.close()
 
+    @pytest.mark.filterwarnings('ignore:.*FigureCanvasAgg is non-interactive.*:UserWarning')
     @cpu_and_gpu
     def test_plot_display_trigger(self, target_device_idx, xp):
         """Test PlotDisplay trigger functionality"""
