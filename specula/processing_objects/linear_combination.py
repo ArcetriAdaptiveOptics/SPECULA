ff526e5bd997fff6826d3fd7f47202d63c67f15b
Guido Agapito <guido.agapito@inaf.it>
2025-09-11 11:09:55 +0200

    (GA) additional options

--- specula/processing_objects/linear_combination.py
+++ specula/processing_objects/linear_combination.py
@@ -6,10 +6,17 @@ from specula.connections import InputList
 
 class LinearCombination(BaseProcessingObj):
     def __init__(self,
+                 no_focus: bool = False,
+                 no_lift: bool = False,
+                 plate_scale_idx: int = None,
                  target_device_idx: int = None, 
                  precision: int = None
                 ):
         super().__init__(target_device_idx=target_device_idx, precision=precision)       
+
+        self.no_focus = no_focus
+        self.no_lift = no_lift
+        self.plate_scale_idx = plate_scale_idx
 
         self.inputs['in_vectors_list'] = InputList(type=BaseValue)
         self.out_vector = BaseValue(target_device_idx=self.target_device_idx)
@@ -17,16 +24,22 @@ class LinearCombination(BaseProcessingObj):
 
     def trigger_code(self):
         in_vectors = self.local_inputs['in_vectors_list']
-        lgs = in_vectors[0].value
-        focus = in_vectors[1].value
-        lift = in_vectors[2].value
-        ngs = in_vectors[3].value
+        idx = 0
+        lgs = in_vectors[idx].value
+        idx += 1
+        if not self.no_focus:
+            focus = in_vectors[idx].value
+            idx += 1
+        if not self.no_lift:
+            lift = in_vectors[idx].value
+            idx += 1
+        ngs = in_vectors[idx].value
 
-        lgs[0:2] = ngs[0:2]
-        lgs[2] = focus[0]
-#        focus *= 0.0
-#        lift *= 0
-#        ngs *= 0
+        lgs[0:2] = ngs[0:1]
+        if not self.no_focus:
+            lgs[2] = focus[0]
+        if self.plate_scale_idx is not None:
+            lgs[self.plate_scale_idx:self.plate_scale_idx+3] = ngs[2:]
 
         self.out_vector.value *= 0.0
         self.out_vector.value[:len(lgs)] = lgs
@@ -36,9 +49,20 @@ class LinearCombination(BaseProcessingObj):
         super().setup()
 
         in_vectors = self.local_inputs['in_vectors_list']
-        lgs = in_vectors[0].value
-        focus = in_vectors[1].value
-        lift = in_vectors[2].value
-        ngs = in_vectors[3].value
+
+        idx = 0
+        lgs = in_vectors[idx].value
+        idx += 1
+        if not self.no_focus:
+            focus = in_vectors[idx].value
+            idx += 1
+        else:
+            focus = self.xp.zeros(0,dtype=self.dtype)
+        if not self.no_lift:
+            lift = in_vectors[idx].value
+            idx += 1
+        else:
+            lift = self.xp.zeros(0,dtype=self.dtype)
+        ngs = in_vectors[idx].value
 
         self.out_vector.value = np.concatenate([lgs, focus, lift, ngs]) * 0.0
