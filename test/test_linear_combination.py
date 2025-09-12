abdeb45ec34c795c5701e90bb6e7647bc6bc5a9c
Guido Agapito <guido.agapito@inaf.it>
2025-09-12 16:38:56 +0200

    (GA) cpu_and_gpu

--- test/test_linear_combination.py
+++ test/test_linear_combination.py
@@ -5,6 +5,7 @@ import unittest
 from specula import cpuArray, np
 from specula.base_value import BaseValue
 from specula.processing_objects.linear_combination import LinearCombination
+from test.specula_testlib import cpu_and_gpu
 from specula.data_objects.simul_params import SimulParams
 
 class TestLinearCombination(unittest.TestCase):
@@ -12,12 +13,18 @@ class TestLinearCombination(unittest.TestCase):
     def setUp(self):
         self.simul_params = SimulParams(pixel_pupil=10, pixel_pitch=1.0, time_step=1)
 
-    def test_basic_combination_no_focus_no_lift(self):
+    @cpu_and_gpu
+    def test_basic_combination_no_focus_no_lift(self, target_device_idx, xp):
         # LGS and NGS only
-        lgs = BaseValue(value=np.array([10., 20., 30., 40., 50.]))
-        ngs = BaseValue(value=np.array([1., 2., 3., 4., 5.]))
+        lgs = BaseValue(value=xp.array([10., 20., 30., 40., 50.]),
+                        target_device_idx=target_device_idx)
+        ngs = BaseValue(value=xp.array([1., 2., 3., 4., 5.]),
+                        target_device_idx=target_device_idx)
         vectors = [lgs, ngs]
-        lc = LinearCombination(self.simul_params, no_focus=True, no_lift=True)
+        lc = LinearCombination(self.simul_params,
+                               no_focus=True,
+                               no_lift=True,
+                               target_device_idx=target_device_idx)
         lc.inputs['in_vectors_list'].set(vectors)
         lc.setup()
         lc.trigger_code()
@@ -28,12 +35,19 @@ class TestLinearCombination(unittest.TestCase):
         # Rest unchanged
         assert out[2] == 30.0
 
-    def test_combination_with_focus(self):
-        lgs = BaseValue(value=np.array([10., 20., 30., 40., 50.]))
-        focus = BaseValue(value=np.array([99.]))
-        ngs = BaseValue(value=np.array([1., 2., 3., 4., 5.]))
+    @cpu_and_gpu
+    def test_combination_with_focus(self, target_device_idx, xp):
+        lgs = BaseValue(value=xp.array([10., 20., 30., 40., 50.]),
+                        target_device_idx=target_device_idx)
+        focus = BaseValue(value=xp.array([99.]),
+                          target_device_idx=target_device_idx)
+        ngs = BaseValue(value=xp.array([1., 2., 3., 4., 5.]),
+                        target_device_idx=target_device_idx)
         vectors = [lgs, focus, ngs]
-        lc = LinearCombination(self.simul_params, no_focus=False, no_lift=True)
+        lc = LinearCombination(self.simul_params,
+                               no_focus=False,
+                               no_lift=True,
+                               target_device_idx=target_device_idx)
         lc.inputs['in_vectors_list'].set(vectors)
         lc.setup()
         lc.trigger_code()
@@ -44,12 +58,19 @@ class TestLinearCombination(unittest.TestCase):
         # Focus copied from focus
         assert out[2] == 99.0
 
-    def test_combination_with_lift(self):
-        lgs = BaseValue(value=np.array([10., 20., 30., 40., 50.]))
-        lift = BaseValue(value=np.array([77.]))
-        ngs = BaseValue(value=np.array([1., 2., 3., 4., 5.]))
+    @cpu_and_gpu
+    def test_combination_with_lift(self, target_device_idx, xp):
+        lgs = BaseValue(value=xp.array([10., 20., 30., 40., 50.]),
+                        target_device_idx=target_device_idx)
+        lift = BaseValue(value=xp.array([77.]),
+                         target_device_idx=target_device_idx)
+        ngs = BaseValue(value=xp.array([1., 2., 3., 4., 5.]),
+                        target_device_idx=target_device_idx)
         vectors = [lgs, lift, ngs]
-        lc = LinearCombination(self.simul_params, no_focus=True, no_lift=False)
+        lc = LinearCombination(self.simul_params,
+                               no_focus=True,
+                               no_lift=False,
+                               target_device_idx=target_device_idx)
         lc.inputs['in_vectors_list'].set(vectors)
         lc.setup()
         lc.trigger_code()
@@ -60,13 +81,21 @@ class TestLinearCombination(unittest.TestCase):
         # Lift is appended at the end
         assert out[-1] == 77.0
 
-    def test_combination_with_focus_and_lift(self):
-        lgs = BaseValue(value=np.array([10., 20., 30., 40., 50.]))
-        focus = BaseValue(value=np.array([99.]))
-        lift = BaseValue(value=np.array([77.]))
-        ngs = BaseValue(value=np.array([1., 2., 3., 4., 5.]))
+    @cpu_and_gpu
+    def test_combination_with_focus_and_lift(self, target_device_idx, xp):
+        lgs = BaseValue(value=xp.array([10., 20., 30., 40., 50.]),
+                        target_device_idx=target_device_idx)
+        focus = BaseValue(value=xp.array([99.]),
+                          target_device_idx=target_device_idx)
+        lift = BaseValue(value=xp.array([77.]),
+                         target_device_idx=target_device_idx)
+        ngs = BaseValue(value=xp.array([1., 2., 3., 4., 5.]),
+                        target_device_idx=target_device_idx)
         vectors = [lgs, focus, lift, ngs]
-        lc = LinearCombination(self.simul_params, no_focus=False, no_lift=False)
+        lc = LinearCombination(self.simul_params,
+                               no_focus=False,
+                               no_lift=False,
+                               target_device_idx=target_device_idx)
         lc.inputs['in_vectors_list'].set(vectors)
         lc.setup()
         lc.trigger_code()
@@ -79,12 +108,20 @@ class TestLinearCombination(unittest.TestCase):
         # Lift is appended at the end
         assert out[-1] == 77.0
 
-    def test_plate_scale_idx(self):
-        lgs = BaseValue(value=np.array([10., 20., 30., 40., 50., 60., 70.]))
-        focus = BaseValue(value=np.array([99.]))
-        ngs = BaseValue(value=np.array([1., 2., 3., 4., 5.]))
+    @cpu_and_gpu
+    def test_plate_scale_idx(self, target_device_idx, xp):
+        lgs = BaseValue(value=xp.array([10., 20., 30., 40., 50., 60., 70.]),
+                        target_device_idx=target_device_idx)
+        focus = BaseValue(value=xp.array([99.]),
+                          target_device_idx=target_device_idx)
+        ngs = BaseValue(value=xp.array([1., 2., 3., 4., 5.]),
+                        target_device_idx=target_device_idx)
         vectors = [lgs, focus, ngs]
-        lc = LinearCombination(self.simul_params, no_focus=False, no_lift=True, plate_scale_idx=3)
+        lc = LinearCombination(self.simul_params,
+                               no_focus=False,
+                               no_lift=True,
+                               plate_scale_idx=3,
+                               target_device_idx=target_device_idx)
         lc.inputs['in_vectors_list'].set(vectors)
         lc.setup()
         lc.trigger_code()
