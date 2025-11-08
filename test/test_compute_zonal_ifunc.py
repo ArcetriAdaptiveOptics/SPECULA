888812d2b65a2148ac20f4e73bb9e4726780859d
Menessao <111561959+Menessao@users.noreply.github.com>
2025-11-08 20:27:01 +0100

    Remove cpu_and_gpu decorator from tests
    Removed cpu_and_gpu decorator from test cases.

--- test/test_compute_zonal_ifunc.py
+++ test/test_compute_zonal_ifunc.py
@@ -6,29 +6,24 @@ import specula
 specula.init(0)
 
 from specula.lib.compute_zonal_ifunc import compute_zonal_ifunc
-from test.specula_testlib import cpu_and_gpu
 
 
 class TestComputeZonalIfunc(unittest.TestCase):
 
-  @cpu_and_gpu
   def test_invalid_geom_raises():
     with pytest.raises(ValueError):
         compute_zonal_ifunc(dim=32, n_act=4, geom='not_a_geom', xp=np)
       
-  @cpu_and_gpu
   def test_double_input_raises():
     with pytest.raises(ValueError):
         compute_zonal_ifunc(dim=32, n_act=4, circ_geom=True, geom='circular', xp=np)
 
-  @cpu_and_gpu
   def test_circular_geom():
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=3, geom='circular')
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot != 19:
           raise ValueError()
 
-  @cpu_and_gpu
   def test_square_geom():
       n_act = 4
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='square')
@@ -36,7 +31,6 @@ class TestComputeZonalIfunc(unittest.TestCase):
       if n_act_tot != n_act**2:
           raise ValueError()
 
-  @cpu_and_gpu
   def test_square_geom():
       n_act = 4
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='alpao')
