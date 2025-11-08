9460b1378ad21f273a8cd33fa719e6d03b2bc66a
Menessao <111561959+Menessao@users.noreply.github.com>
2025-11-08 20:35:25 +0100

    Trust me, I know how classes work

--- test/test_compute_zonal_ifunc.py
+++ test/test_compute_zonal_ifunc.py
@@ -10,28 +10,28 @@ from specula.lib.compute_zonal_ifunc import compute_zonal_ifunc
 
 class TestComputeZonalIfunc(unittest.TestCase):
 
-  def test_invalid_geom_raises():
+  def test_invalid_geom_raises(self):
     with pytest.raises(ValueError):
         compute_zonal_ifunc(dim=32, n_act=4, geom='not_a_geom', xp=np)
       
-  def test_double_input_raises():
+  def test_double_input_raises(self):
     with pytest.raises(ValueError):
         compute_zonal_ifunc(dim=32, n_act=4, circ_geom=True, geom='circular', xp=np)
 
-  def test_circular_geom():
+  def test_circular_geom(self):
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=3, geom='circular')
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot != 19:
           raise ValueError()
 
-  def test_square_geom():
+  def test_square_geom(self):
       n_act = 4
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='square')
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot != n_act**2:
           raise ValueError()
 
-  def test_square_geom():
+  def test_square_geom(self):
       n_act = 4
       ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='alpao')
       n_act_tot = np.shape(ifs_cube)[0]
