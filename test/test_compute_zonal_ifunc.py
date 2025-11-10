056cd61e2bf7180a90cb7369552b8ce19ebcf151
Menessao <111561959+Menessao@users.noreply.github.com>
2025-11-10 11:05:41 +0100

    Update test zonal ifunc to test CPU and GPU execution
    Refactor tests to support CPU and GPU execution by adding a decorator. Update calls to compute_zonal_ifunc to include 'xp' and 'dtype' parameters.

--- test/test_compute_zonal_ifunc.py
+++ test/test_compute_zonal_ifunc.py
@@ -6,34 +6,40 @@ import specula
 specula.init(0)
 
 from specula.lib.compute_zonal_ifunc import compute_zonal_ifunc
+from test.specula_testlib import cpu_and_gpu
 
 
 class TestComputeZonalIfunc(unittest.TestCase):
 
-  def test_invalid_geom_raises(self):
+  @cpu_and_gpu
+  def test_invalid_geom_raises(self, xp):
     with pytest.raises(ValueError):
-        compute_zonal_ifunc(dim=32, n_act=4, geom='not_a_geom', xp=np)
+        compute_zonal_ifunc(dim=32, n_act=4, geom='not_a_geom', xp=xp, dtype=xp.float32)
       
-  def test_double_input_raises(self):
+  @cpu_and_gpu
+  def test_double_input_raises(self, xp):
     with pytest.raises(ValueError):
-        compute_zonal_ifunc(dim=32, n_act=4, circ_geom=True, geom='circular', xp=np)
-
-  def test_circular_geom(self):
-      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=3, geom='circular')
+        compute_zonal_ifunc(dim=32, n_act=4, circ_geom=True, geom='circular', xp=xp, dtype=xp.float32)
+      
+  @cpu_and_gpu
+  def test_circular_geom(self, xp):
+      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=3, geom='circular', xp=xp, dtype=xp.float32)
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot != 19:
           raise ValueError()
 
-  def test_square_geom(self):
+  @cpu_and_gpu
+  def test_square_geom(self, xp):
       n_act = 4
-      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='square')
+      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='square', xp=xp, dtype=xp.float32)
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot != n_act**2:
           raise ValueError()
 
-  def test_square_geom(self):
+  @cpu_and_gpu
+  def test_square_geom(self, xp):
       n_act = 4
-      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='alpao')
+      ifs_cube,_ = compute_zonal_ifunc(dim=32, n_act=n_act, geom='alpao', xp=xp, dtype=xp.float32)
       n_act_tot = np.shape(ifs_cube)[0]
       if n_act_tot >= n_act**2:
           raise ValueError()
