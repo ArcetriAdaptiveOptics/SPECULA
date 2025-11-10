Fixed n_act_tot computation in compute_zonal_ifunc

specula/lib/compute_zonal_ifunc.py
@@ -34,6 +34,7 @@ def compute_zonal_ifunc(dim, n_act, xp=np, dtype=np.float32, circ_geom=False,  a
             step *= float(n_act) / float(n_act - 1)
             na = xp.arange(xp.ceil(n_act / 2)) * 6
         na[0] = 1  # The first value is always 1
+        n_act_tot = xp.sum(na)
         pol_coords = xp.zeros((2, n_act_tot))
         ka = 0
         # Refactor this!
@@ -57,16 +58,17 @@ def compute_zonal_ifunc(dim, n_act, xp=np, dtype=np.float32, circ_geom=False,  a
         rho_max = (dim*(9/8-n_act/(24*16)))/2 # slightly larger than dim, depends on n_act
         x = x[rho<=rho_max]
         y = y[rho<=rho_max]
+        n_act_tot = xp.size(rho)
       
     elif geom == 'square': # default
         x, y = xp.meshgrid(xp.linspace(0, dim, n_act), xp.linspace(0, dim, n_act))
         x, y = x.ravel(), y.ravel()
+        n_act_tot = n_act**2
       
     else:
       raise ValueError("Unrecognized geometry type! Avaliable types are: 'circular', 'alpao', 'square'")
 
     coordinates = xp.vstack((x, y))
-    n_act_tot = xp.shape(coordinates)[1]
     grid_x, grid_y = xp.meshgrid(xp.arange(dim), xp.arange(dim))
 
     # ----------------------------------------------------------