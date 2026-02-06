a1b93ae4eafaae16e15cea475f0b72989063335a
menessao <menessinimatteo@gmail.com>
2026-02-06 21:01:50 +0100

    Added xp and dtype inputs to radial_profile

--- specula/lib/radial_profile.py
+++ specula/lib/radial_profile.py
@@ -1,40 +1,40 @@
 import numpy as np
 
-def computeRadialProfile(image, centerInPxY=None, centerInPxX=None):
+def computeRadialProfile(image, centerInPxY=None, centerInPxX=None, xp=np, dtype=np.float64):
     if centerInPxX is None:
         centerInPxX = image.shape[1]/2
     if centerInPxY is None:
         centerInPxY = image.shape[0]/2
-    yCoord, xCoord= np.indices(image.shape)
+    yCoord, xCoord= xp.indices(image.shape)
     yCoord= (yCoord - centerInPxY)
     xCoord= (xCoord - centerInPxX)
-    rCoord=np.sqrt(xCoord**2 + yCoord**2)
-    indexR= np.argsort(rCoord.flat)
+    rCoord=xp.sqrt(xCoord**2 + yCoord**2)
+    indexR= xp.argsort(rCoord.flat)
     radialDistancesSorted= rCoord.flat[indexR]
     imageValuesSortedByRadialDistance= image.flat[indexR]
-    integerPartOfRadialDistances= radialDistancesSorted.astype(np.int64)
+    integerPartOfRadialDistances= radialDistancesSorted.astype(dtype)
     deltaRadialDistance= integerPartOfRadialDistances[1:] - \
         integerPartOfRadialDistances[:-1]
-    radialDistanceChanges= np.where(deltaRadialDistance)[0]
+    radialDistanceChanges= xp.where(deltaRadialDistance)[0]
     nPxInBinZero= radialDistanceChanges[0]+ 1
     nPxInRadialBin= radialDistanceChanges[1:] - \
         radialDistanceChanges[:-1]
-    imageRadialCumSum= np.cumsum(imageValuesSortedByRadialDistance,
-                                 dtype=np.float64)
+    imageRadialCumSum= xp.cumsum(imageValuesSortedByRadialDistance,
+                                 dtype=dtype)
     imageSumInBinZero= imageRadialCumSum[radialDistanceChanges[0]]
     imageSumInRadialBin= \
         imageRadialCumSum[radialDistanceChanges[1:]] - \
         imageRadialCumSum[radialDistanceChanges[:-1]]
     profileInZero= imageSumInBinZero / nPxInBinZero
     profileFromOne= imageSumInRadialBin / nPxInRadialBin
-    profile= np.hstack([profileInZero, profileFromOne])
+    profile= xp.hstack([profileInZero, profileFromOne])
 
-    distanceRadialCumSum= np.cumsum(radialDistancesSorted)
+    distanceRadialCumSum= xp.cumsum(radialDistancesSorted)
     distanceSumInBinZero= distanceRadialCumSum[radialDistanceChanges[0]]
     distanceSumInRadialBin= \
         distanceRadialCumSum[radialDistanceChanges[1:]] - \
         distanceRadialCumSum[radialDistanceChanges[:-1]]
     distanceInZero= distanceSumInBinZero / nPxInBinZero
     distanceFromOne= distanceSumInRadialBin / nPxInRadialBin
-    radialDistance= np.hstack([distanceInZero, distanceFromOne])
+    radialDistance= xp.hstack([distanceInZero, distanceFromOne])
     return profile, radialDistance
