59b514673ede920124eb3ce0090ad1f03657929e
Guido Agapito <guido.agapito@inaf.it>
2026-03-08 16:12:08 +0100

    (GA) update

--- specula/processing_objects/atmo_infinite_evolution.py
+++ specula/processing_objects/atmo_infinite_evolution.py
@@ -85,9 +85,9 @@ class AtmoInfiniteEvolution(BaseProcessingObj):
         self.ref_wavelengthInNm = 500
 
         if not hasattr(extra_delta_time,"__len__"):
-            self.extra_delta_time = cpuArray(self.n_infinite_phasescreens*[extra_delta_time])
+            self.extra_delta_time = self.n_infinite_phasescreens*[extra_delta_time]
         else:
-            self.extra_delta_time = cpuArray(extra_delta_time)
+            self.extra_delta_time = extra_delta_time
 
         self.inputs['seeing'] = InputValue(type=BaseValue)
         self.inputs['wind_speed'] = InputValue(type=BaseValue)
