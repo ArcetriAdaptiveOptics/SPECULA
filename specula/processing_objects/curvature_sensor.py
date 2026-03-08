7a2eae4a4b5356b871ee6f5d09baa960fc155b34
Guido Agapito <guido.agapito@inaf.it>
2026-03-08 09:02:23 +0100

    (GA) update

--- specula/processing_objects/curvature_sensor.py
+++ specula/processing_objects/curvature_sensor.py
@@ -8,6 +8,7 @@ from specula.lib.zernike_generator import ZernikeGenerator
 from specula.lib.extrapolation_2d import EFInterpolator
 from specula.lib.mask import CircularMask
 from specula.lib.make_mask import make_mask
+from specula.lib.toccd import toccd
 
 @fuse(kernel_name='abs2_cwfs')
 def abs2_cwfs(u_fp, out, xp):
@@ -25,6 +26,7 @@ class CurvatureSensor(BaseProcessingObj):
                  pxscale: float,
                  output_resolution: int,
                  defocus_rms_nm: float,
+                 fov_ovs_coeff: float = 2.0,
                  target_device_idx: int = None,
                  precision: int = None):
 
@@ -34,13 +36,14 @@ class CurvatureSensor(BaseProcessingObj):
         self.pxscale = pxscale
         self.output_resolution = output_resolution
         self.defocus_rms_nm = defocus_rms_nm
+        self.fov_ovs_coeff = max(1.0, fov_ovs_coeff)
 
         self.inputs['in_ef'] = InputValue(type=ElectricField)
 
-        # Output: Two intensities (Intra- and Extra-focal)
-        self._out_i1 = Intensity(self.output_resolution, self.output_resolution, 
+        # Final requested outputs
+        self._out_i1 = Intensity(self.output_resolution, self.output_resolution,
                                  precision=self.precision, target_device_idx=self.target_device_idx)
-        self._out_i2 = Intensity(self.output_resolution, self.output_resolution, 
+        self._out_i2 = Intensity(self.output_resolution, self.output_resolution,
                                  precision=self.precision, target_device_idx=self.target_device_idx)
         self.outputs['out_i1'] = self._out_i1
         self.outputs['out_i2'] = self._out_i2
@@ -52,83 +55,127 @@ class CurvatureSensor(BaseProcessingObj):
         self.ef_interpolator = None
         self.ef_resampled = None
         self.detector_mask = None
+        self._temp_i1 = None
+        self._temp_i2 = None
+        self.actual_internal_pxscale = None
+
+        self.internal_res = 0
 
     def setup(self):
         super().setup()
         in_ef = self.local_inputs['in_ef']
 
-        # 1. Calculate magnification to achieve the requested pixel scale after FFT
-        # Spatial resolution in the FFT plane is dx = lambda / D_padded
+        # 1. Convert pxscale from arcsec to radians
+        # dx_focal = lambda / (N_pad * dx_pupil) -> N_pad = lambda / (dx_focal * dx_pupil)
         pxscale_rad = self.pxscale / RAD2ASEC
-        required_dx = (self.wavelength_in_nm * 1e-9) / (self.output_resolution * pxscale_rad)
-        self.magnification = in_ef.pixel_pitch / required_dx
+        lambda_m = self.wavelength_in_nm * 1e-9
 
-        # Setup EFInterpolator
+        # Theoretical size to achieve the *exact* requested pxscale at the output
+        # (before cropping)
+        exact_fft_size = lambda_m / (pxscale_rad * in_ef.pixel_pitch)
+
+        # Oversampling is needed to avoid aliasing when cropping to the wanted FoV,
+        # especially for small FoVs.
+        internal_size_float = exact_fft_size * self.fov_ovs_coeff
+        self.internal_res = int(np.ceil(internal_size_float))
+        self.internal_res += (self.internal_res % 2) # Force even
+
+        # Recalculate the "actual" internal pxscale based on the integer size,
+        # which will be important when we do toccd to rescale to the final output_resolution
+        self.actual_internal_pxscale = lambda_m / (self.internal_res * in_ef.pixel_pitch) * RAD2ASEC
+
+        # 2. Setup EFInterpolator
         self.ef_interpolator = EFInterpolator(
             in_ef=in_ef,
-            out_shape=(self.output_resolution, self.output_resolution),
-            magnification=self.magnification,
+            out_shape=(self.internal_res, self.internal_res),
+            magnification=1.0,
+            use_out_ef_cache=False, 
             target_device_idx=self.target_device_idx,
             precision=self.precision
         )
 
-        self.ef_resampled = self.xp.zeros((self.output_resolution, self.output_resolution),
+        # Allocate internal working arrays
+        self.ef_resampled = self.xp.zeros((self.internal_res, self.internal_res),
                                           dtype=self.complex_dtype)
-        self.fp_plus = self.xp.zeros((self.output_resolution, self.output_resolution),
+        self.fp_plus = self.xp.zeros((self.internal_res, self.internal_res),
                                      dtype=self.complex_dtype)
-        self.fp_minus = self.xp.zeros((self.output_resolution, self.output_resolution),
+        self.fp_minus = self.xp.zeros((self.internal_res, self.internal_res),
                                       dtype=self.complex_dtype)
 
-        # 2. Define the exact area occupied by the pupil in the padded array
-        pupil_diameter_pix = in_ef.size[0] * self.magnification
-        center = np.ones(2, dtype=self.dtype) * (self.output_resolution / 2.0)
+        # Temporary intensity arrays for the full padded FoV
+        self._temp_i1 = self.xp.zeros((self.internal_res, self.internal_res), dtype=self.dtype)
+        self._temp_i2 = self.xp.zeros((self.internal_res, self.internal_res), dtype=self.dtype)
 
-        # Generate a mask so that the ZernikeGenerator creates the parabola exactly
-        # confined to the real diameter of the scaled pupil
-        mask = CircularMask((self.output_resolution, self.output_resolution), 
+        # 3. Create Aberration Phase (Focus)
+        new_pixel_pitch = in_ef.pixel_pitch * (in_ef.size[0] / self.internal_res)
+        pupil_diameter_m = in_ef.size[0] * in_ef.pixel_pitch
+        pupil_diameter_pix = pupil_diameter_m / new_pixel_pitch
+
+        center = np.ones(2, dtype=self.dtype) * (self.internal_res / 2.0)
+        mask = CircularMask((self.internal_res, self.internal_res),
                             maskCenter=center, maskRadius=pupil_diameter_pix / 2.0)
 
         zgen = ZernikeGenerator(mask, self.xp, self.dtype)
-        z4 = zgen.getZernike(4) # Z4 Noll = Focus
+        z4 = zgen.getZernike(4)
 
-        # Convert RMS Nanometers to Phase Radians
         k = 2.0 * np.pi / self.wavelength_in_nm
         phase_aberration = z4 * self.defocus_rms_nm * k
 
         self.exp_plus = self.xp.exp(1j * phase_aberration, dtype=self.complex_dtype)
         self.exp_minus = self.xp.exp(-1j * phase_aberration, dtype=self.complex_dtype)
 
-        # 3. Create a Field Stop mask based on wanted_fov
-        fov_pixels = self.wanted_fov / self.pxscale
-        self.detector_mask = make_mask(self.output_resolution,
-                                       diaratio=fov_pixels / self.output_resolution,
+        # 4. Create a Field Stop mask based on wanted_fov on the INTERNAL grid
+        fov_pixels = self.wanted_fov / self.actual_internal_pxscale
+        self.detector_mask = make_mask(self.internal_res,
+                                       diaratio=fov_pixels / self.internal_res,
                                        xp=self.xp)
 
-    def trigger_code(self):
-        # 1. Retrieve and interpolate input electric field to the new padded grid
-        self.ef_interpolator.interpolate()
-        self.ef_interpolator.interpolated_ef().ef_at_lambda(self.wavelength_in_nm,
-                                                            out=self.ef_resampled)
 
-        # 2. Intrafocal propagation
+    def prepare_trigger(self, t):
+        super().prepare_trigger(t)
+        self.ef_interpolator.interpolate()
+        self.ef_interpolator.interpolated_ef().ef_at_lambda(
+            self.wavelength_in_nm, out=self.ef_resampled)
+
+
+    def trigger_code(self):
+        # Intrafocal propagation
         ef_plus = self.ef_resampled * self.exp_plus
         self.fp_plus[:] = self.xp.fft.fftshift(self.xp.fft.fft2(ef_plus))
-        abs2_cwfs(self.fp_plus, self._out_i1.i, xp=self.xp)
-        self._out_i1.i *= self.detector_mask
+        abs2_cwfs(self.fp_plus, self._temp_i1, xp=self.xp)
+        self._temp_i1 *= self.detector_mask
 
-        # 3. Extrafocal propagation
+        # Extrafocal propagation
         ef_minus = self.ef_resampled * self.exp_minus
         self.fp_minus[:] = self.xp.fft.fftshift(self.xp.fft.fft2(ef_minus))
-        abs2_cwfs(self.fp_minus, self._out_i2.i, xp=self.xp)
-        self._out_i2.i *= self.detector_mask
+        abs2_cwfs(self.fp_minus, self._temp_i2, xp=self.xp)
+        self._temp_i2 *= self.detector_mask
+
+        # Crop to Field of View
+        # We need to extract the area defined by wanted_fov
+        wanted_fov_internal_pixels = int(np.ceil(self.wanted_fov / self.actual_internal_pxscale))
+        cut_start = (self.internal_res - wanted_fov_internal_pixels) // 2
+        cut_end = cut_start + wanted_fov_internal_pixels
+
+        i1_fov = self._temp_i1[cut_start:cut_end, cut_start:cut_end]
+        i2_fov = self._temp_i2[cut_start:cut_end, cut_start:cut_end]
+
+        # Bin/Resample to the exact requested output_resolution using toccd
+        # toccd handles the exact re-binning/interpolation preserving flux
+        self._out_i1.i[:] = toccd(i1_fov,
+                                  (self.output_resolution, self.output_resolution),
+                                  xp=self.xp)
+        self._out_i2.i[:] = toccd(i2_fov,
+                                  (self.output_resolution, self.output_resolution),
+                                  xp=self.xp)
+
 
     def post_trigger(self):
         super().post_trigger()
-        # Photometric normalization
+
         in_ef = self.local_inputs['in_ef']
         phot = in_ef.S0 * in_ef.masked_area()
 
-        # Avoid division by zero if image is empty
         sum1 = self._out_i1.i.sum()
         sum2 = self._out_i2.i.sum()
 
