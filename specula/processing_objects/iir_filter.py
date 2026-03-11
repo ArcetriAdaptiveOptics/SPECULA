e47a343d8627bbafe9fe9f601c6d444eebb46f88
Alfio Puglisi <alfio.puglisi@inaf.it>
2026-03-11 14:44:42 +0100

    Optimized set_gain, added Dynamic objects

--- specula/processing_objects/iir_filter.py
+++ specula/processing_objects/iir_filter.py
@@ -1,9 +1,6 @@
 from specula.processing_objects.base_filter import BaseFilter
 from specula.data_objects.iir_filter_data import IirFilterData
 from specula.data_objects.simul_params import SimulParams
-from specula.connections import InputValue
-from specula.base_value import BaseValue
-from specula import cpuArray
 
 
 class IirFilter(BaseFilter):
@@ -60,23 +57,6 @@ class IirFilter(BaseFilter):
         self._den_mask = self.xp.ones_like(self.iir_filter_data.den)
         if not integration:
             self._den_mask[:, :-1] = 0
-
-        self.inputs['reset'] = InputValue(type=BaseValue, optional=True)
-        self.inputs['int_gain'] = InputValue(type=BaseValue, optional=True)
-
-    def prepare_trigger(self, t):
-        super().prepare_trigger(t)
-
-        # Reset internal state
-        reset_input = self.local_inputs['reset']
-        if reset_input is not None and reset_input.generation_time == self.current_time:
-            self.reset_states()
-
-        # Update internal IIR filter data if gain input changes
-        gain_input = self.local_inputs['int_gain']
-        if gain_input is not None and gain_input.generation_time == self.current_time:
-            int_gain = cpuArray(gain_input.get_value())
-            self.iir_filter_data.set_gain(int_gain)
 
     def trigger_code(self):
         """IIR filter computation."""
