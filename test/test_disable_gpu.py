fc5517b7ed2cc9536515ffd5286c3ff0dabd5437
Alfio Puglisi <alfio.puglisi@inaf.it>
2026-04-18 19:13:57 +0200

    Fixed test

--- test/test_disable_gpu.py
+++ test/test_disable_gpu.py
@@ -2,6 +2,7 @@ import specula
 specula.init(0)  # Default target device
 
 import os
+import sys
 import numpy as np
 import unittest
 import importlib
@@ -14,7 +15,8 @@ class TestDisableGpu(unittest.TestCase):
     def test_disable_gpu(self):
         '''Test that SPECULA_DISABLE_GPU always results in numpy being loaded instead of cupy'''
 
-        previous = os.environ.get('SPECULA_DISABLE_GPU', None)
+        orig_disable = os.environ.get('SPECULA_DISABLE_GPU', None)
+        orig_cupy = sys.modules.get('cupy', None)
 
         try:
             with mock.patch.dict("sys.modules", {
@@ -25,5 +27,7 @@ class TestDisableGpu(unittest.TestCase):
                 specula.init(0)
                 assert specula.xp == np
         finally:
-            if previous is not None:
+            if orig_disable is not None:
                 os.environ['SPECULA_DISABLE_GPU'] = previous
+            if orig_cupy is not None:
+                sys.modules['cupy'] = orig_cupy
