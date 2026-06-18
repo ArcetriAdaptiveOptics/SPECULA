93dcd674009a69a073cdecdd53fdcb736e9da7d4
Menessao <menessinimatteo@gmail.com>
2026-06-18 11:11:34 +0200

    Fixed typo

--- specula/processing_objects/dm.py
+++ specula/processing_objects/dm.py
@@ -180,7 +180,7 @@ class DM(BaseProcessingObj):
     @classmethod
     def output_names(cls):
         return {'out_layer': OutputDesc(Layer, 'Output wavefront layer produced by the DM'),
-                'out_clip_command': OutputDesc(Layer, 'DM applied command, after (optional) clipping')}
+                'out_clipped_command': OutputDesc(Layer, 'DM applied command, after (optional) clipping')}
 
     def trigger_code(self):
         input_commands = self.local_inputs['in_command'].value
